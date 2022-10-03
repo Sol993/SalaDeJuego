3d5c85cbf655b21ef751f8d9a68cb966be2e6d6e
Solange Correa <sol993@gmail.com>
2022-10-03 20:40:30 -0300

    Se termino el preguntado de pais

--- src/app/modulojuego/preguntado/preguntado.component.ts
+++ src/app/modulojuego/preguntado/preguntado.component.ts
@@ -12,23 +12,58 @@ export class PreguntadoComponent implements OnInit {
   opciones: Array<any> = [];
   respuesta: any;
   cantidadOpciones: number = 4;
+  vidas: number = 3;
+  puntaje: number = 0;
+  gano = false;
+  perdio = false;
 
   constructor(private _service: PaisesService) {}
 
   ngOnInit() {
     this._service.getCountries().subscribe((response) => {
       this.paises = response;
-
-      this.respuesta =
-        this.paises[Math.floor(Math.random() * this.paises.length)];
-      console.log(this.respuesta);
-      this.opciones.push(this.respuesta);
-      for (let i = 0; i < this.cantidadOpciones; i++) {
-        let respuestaErronea =
-          this.paises[Math.floor(Math.random() * this.paises.length)];
-        this.opciones.push(respuestaErronea);
-      }
+      this.iniciarJuego();
     });
+  }
+
+  comprobar(pais: any) {
+    if (pais.name.trim() == this.respuesta.name.trim()) {
+      this.puntaje++;
+    } else {
+      this.vidas--;
+    }
+
+    if (this.vidas == 0) {
+     this.perdio=true;
+    } else {
+      this.iniciarJuego();
+    }
+    if (this.puntaje == 250) {
+      this.gano=true;
+    }
+  }
+
+  iniciarJuego() {
+    this.opciones = [];
+
+    this.respuesta =
+      this.paises[Math.floor(Math.random() * this.paises.length)];
+    this.opciones.push(this.respuesta);
+    this.paises.pop(this.respuesta);
+
+    for (let i = 0; i <= this.cantidadOpciones; i++) {
+      let respuestaErronea =
+        this.paises[Math.floor(Math.random() * this.paises.length)];
+      this.opciones.push(respuestaErronea);
+    }
+
+    for (var i = this.opciones.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+
+      var temp = this.opciones[i];
+      this.opciones[i] = this.opciones[j];
+      this.opciones[j] = temp;
+    }
   }
   reiniciarJuego(){
     location.href = 'juegos/preguntado';
